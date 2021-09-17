40b0c8ab43d8c49b0b53278d046432015ba7b28b
Otavio Augusto <otavio_falcao654@hotmail.com>
2021-09-16 23:01:11 -0300

    Atividade 1 e 2 Javascript

---
+++ script.js
@@ -0,0 +1,50 @@
+//      ATIVIDADE 1 
+
+//1) Criar variáveis com tipos de dados:
+
+//a) Number
+
+var numero = 10;
+
+// b) String
+
+var nome = "João";
+
+// c) Boolean
+
+var vedade = true;
+var mentira = false;
+
+// d) Array
+
+var frutas = ["maçã", "pera", "uva"];
+
+// e) Object
+
+var carro = {cor:"preto", modelo: "XC60",marca:"Volvo", ano: 2020};
+
+// f) Undefined
+
+var a;
+
+// g) Null 
+
+var nul = null;
+
+
+//           ATIVIDADE 2
+
+//1)      Criar duas variáveis usando a palavra-chave var.
+
+ var nome = "João";
+ var animal = "cachorro";
+
+//2)      Criar duas variáveis usando a palavra-chave let.
+
+let idade = 18;
+let ano = 2021;
+
+//3)      Criar duas constantes usando a palavra-chave const.
+
+const cidade = "Recife";
+const PI = 3.14159;
